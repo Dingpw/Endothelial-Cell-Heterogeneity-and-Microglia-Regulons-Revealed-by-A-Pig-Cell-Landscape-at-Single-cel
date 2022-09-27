d9710e8f378446f3619fc3cca58e4374fee16eed
Dingpw <42132756+Dingpw@users.noreply.github.com>
2022-09-27 21:01:14 +0800

    Update step01_snRNA_Index.sh

--- step01_snRNA_Index.sh
+++ step01_snRNA_Index.sh
@@ -1,1 +1,3 @@
+## STAR version 2.7.1a (https://github.com/alexdobin/STAR/tree/2.7.1a)
+
 STAR --runThreadN 3 --runMode genomeGenerate --genomeDir index --genomeFastaFiles Sus_scrofa.Sscrofa11.1.dna.toplevel.fa --sjdbGTFfile ref/Sus_scrofa.Sscrofa11.1.Release_99.gtf --sjdbOverhang 99
