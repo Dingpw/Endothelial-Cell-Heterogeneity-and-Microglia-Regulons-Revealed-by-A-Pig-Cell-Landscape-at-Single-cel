d2af52a254186c438f5716596717e7bc1a468b93
Dingpw <42132756+Dingpw@users.noreply.github.com>
2022-09-27 21:16:51 +0800

    Create step01_scRNA_pre.sh

---
+++ step01_scRNA_pre.sh
@@ -0,0 +1,19 @@
+## CellRanger version 3.0.2 (https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/what-is-cell-ranger)
+/Software/cellranger-3.0.2_R/cellranger mkgtf Sus_scrofa.Sscrofa11.1.96.gtf Sus_scrofa.Sscrofa11.1.96.filtered.gtf \
+                  --attribute=gene_biotype:protein_coding \
+                  --attribute=gene_biotype:lincRNA \
+                  --attribute=gene_biotype:antisense \
+                  --attribute=gene_biotype:IG_LV_gene \
+                  --attribute=gene_biotype:IG_V_gene \
+                  --attribute=gene_biotype:IG_V_pseudogene \
+                  --attribute=gene_biotype:IG_D_gene \
+                  --attribute=gene_biotype:IG_J_gene \
+                  --attribute=gene_biotype:IG_J_pseudogene \
+                  --attribute=gene_biotype:IG_C_gene \
+                  --attribute=gene_biotype:IG_C_pseudogene \
+                  --attribute=gene_biotype:TR_V_gene \
+                  --attribute=gene_biotype:TR_V_pseudogene \
+                  --attribute=gene_biotype:TR_D_gene \
+                  --attribute=gene_biotype:TR_J_gene \
+                  --attribute=gene_biotype:TR_J_pseudogene \
+                  --attribute=gene_biotype:TR_C_gene
