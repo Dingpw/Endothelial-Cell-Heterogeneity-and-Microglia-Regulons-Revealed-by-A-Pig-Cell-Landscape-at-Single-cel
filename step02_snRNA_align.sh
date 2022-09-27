d0936888108cfb1a37387c932000556100e0812c
Dingpw <42132756+Dingpw@users.noreply.github.com>
2022-09-27 21:58:37 +0800

    Update step02_snRNA_align.sh

--- step02_snRNA_align.sh
+++ step02_snRNA_align.sh
@@ -1,7 +1,8 @@
 ## You should download and install these three tools before running this step:
-## Cromwell release 35 (https://github.com/broadinstitute/cromwell/releases/tag/35)
+## Cromwell release 35 (https://github.com/broadinstitute/cromwell/)
 ## DNBelab_C_Series_HT_scRNA-analysis-software (https://github.com/MGI-tech-bioinformatics/DNBelab_C_Series_HT_scRNA-analysis-software)
-## PISA v0.5 (https://github.com/shiquan/PISA/)
+## PISA version 0.5 (https://github.com/shiquan/PISA/)
+## R version 3.5.2 (https://www.r-project.org/). R packages: “ggplot2”, “getopt”, “data.table”, “cowplot”, “DropletUtils”, and “Seurat”.
 
 ## "Droplet_scRNA_pipeline_html.wdl" file was stored in pipeline "DNBelab_C_Series_HT_scRNA-analysis-software"
 java -jar tools/DNBelab_C_Series_HT_scRNA-analysis-software/cromwell-35.jar run -i config.json pipelines/Droplet_scRNA_pipeline_html.wdl
